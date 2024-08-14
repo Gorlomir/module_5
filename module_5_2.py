844ffdf2a5431d5ba9fe637a95fffb14abdb405b
Gorlomir <151527759+Gorlomir@users.noreply.github.com>
2024-08-14 21:47:35 +0500

    Update module_5_2.py

--- module_5_2.py
+++ module_5_2.py
@@ -15,10 +15,15 @@ class House:
     def __str__(self):
         return f"Название: {self.name}, кол-во этажей: {self.number_of_floors}"
 
-h1 = House('ЖК Горский', 18)
-h2 = House('Домик в деревне', 2)
+h1 = House('ЖК Горский', 10)
+h2 = House('Домик в деревне', 20)
 h1.go_to(5)
 h2.go_to(10)
 
+# __str__
+print(h1)
+print(h2)
+
+# __len__
 print(len(h1))
-print(str(h1))
+print(len(h2))
